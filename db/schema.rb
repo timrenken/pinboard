4908181d8285b86443a3daf1fce8dc5d5bca7f50
Tim Renken <tim@timrenken.io>
2015-02-24 19:18:12 -0600

    updates a few pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224025845) do
+ActiveRecord::Schema.define(version: 20150224223324) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "pin_id"
@@ -34,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150224025845) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "category_id"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
